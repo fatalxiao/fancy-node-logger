d1d9171752338db41891976914a08072883196b7
liangxiaojun <fatalxiao@163.com>
2021-06-09 15:02:23 +0800

    update

--- examples/base.js
+++ examples/base.js
@@ -2,6 +2,17 @@
 
 const FancyNodeLogger = require('../dist');
 
+// Without Title
+FancyNodeLogger.note('Note Message');
+FancyNodeLogger.verbose('Verbose Message');
+FancyNodeLogger.info('Infomation Message');
+FancyNodeLogger.success('Success Message');
+FancyNodeLogger.done('Done Message');
+FancyNodeLogger.warning('Warning Message');
+FancyNodeLogger.error('Error Message');
+FancyNodeLogger.failure('Failure Message');
+
+// With Title
 FancyNodeLogger.note('Note Title', 'Note Message');
 FancyNodeLogger.verbose('Verbose Title', 'Verbose Message');
 FancyNodeLogger.info('Infomation Title', 'Infomation Message');
