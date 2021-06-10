f9c755da65a4dd6244979f0f167352be2a6786ba
liangxiaojun <fatalxiao@163.com>
2021-06-10 16:16:15 +0800

    update

--- examples/customized.js
+++ examples/customized.js
@@ -12,8 +12,6 @@ const buildSuccessLogger = fancyNodeLogger.createLogger(fancyNodeLogger.Type.SUC
 buildSuccessLogger('Build Success Title', 'Build Success Message\n');
 
 // regist the self defined logger
-fancyNodeLogger.registLogger(
-    'buildError',
-    fancyNodeLogger.createLogger(fancyNodeLogger.Type.ERROR, 'BUILD ERROR')
-);
+const buildErrorLogger = fancyNodeLogger.createLogger(fancyNodeLogger.Type.ERROR, 'BUILD ERROR');
+fancyNodeLogger.registLogger('buildError', buildErrorLogger);
 fancyNodeLogger.buildError('Build Error Title', 'Build Error Message\n');
